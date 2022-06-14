44a130a31933493d06803e1ace759014b085c479
lvcc2018 <thulvcc2017@gmail.com>
2022-06-15 00:49:46 +0800

    Modify

--- utils.py
+++ utils.py
@@ -13,48 +13,6 @@ def dump_file(obj:list, file_path:str):
 def load_file(file_path:str)->list:
     return [json.loads(i.strip()) for i in open(file_path).readlines()]
 
-'''
-def preprocess(dataset):
-    train_data = []
-    valid_data = []
-    test_data = []
-    for lang in ['en', 'zh', 'es', 'fr']:
-        train_data_temp = {0:[], 1:[], 2:[]}
-        for i in range(len(dataset[lang]['train'])):
-            train_data_temp[dataset[lang]['train'][i]['label']].append(dataset[lang]['train'][i])
-        temp = []
-        for i in [0,1,2]:
-            random.shuffle(train_data_temp[i])
-            temp += train_data_temp[i][:1000]
-        train_data_temp = temp
-        for i in train_data_temp:
-            i['language'] = lang
-        random.shuffle(train_data_temp)
-        train_data += train_data_temp
-        valid_data_temp = {0:[], 1:[], 2:[]}
-        for i in range(len(dataset[lang]['validation'])):
-            valid_data_temp[dataset[lang]['validation'][i]['label']].append(dataset[lang]['validation'][i])
-        temp = []
-        for i in [0,1,2]:
-            random.shuffle(valid_data_temp[i])
-            temp += valid_data_temp[i][:200]
-        valid_data_temp = temp
-        for i in valid_data_temp:
-            i['language'] = lang
-        random.shuffle(valid_data_temp)
-        valid_data += valid_data_temp
-        test_data_temp = [dataset[lang]['test'][i] for i in range(len(dataset[lang]['test']))]
-        for i in test_data_temp:
-            i['language'] = lang
-        random.shuffle(test_data_temp)
-        test_data += test_data_temp
-    random.shuffle(train_data)
-    random.shuffle(valid_data)
-    random.shuffle(test_data)
-    dump_file(train_data, '/home/lvcc/mT5-baselines/data/XNLI/train.json')
-    dump_file(valid_data, '/home/lvcc/mT5-baselines/data/XNLI/valid.json')
-    dump_file(test_data, '/home/lvcc/mT5-baselines/data/XNLI/test.json')
-'''
 
 def get_args():
     parser = argparse.ArgumentParser()
